2cf6e192170c74c3c0143335a84784f33e9ba3cd
Relaby <64512296+Relaby@users.noreply.github.com>
2024-02-18 00:02:48 -0500

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ BOT_TOKEN = "Bot token"
 TWITCH_URL = "https://twitch.tv/relabyv" # leave blank if you dont have a twitch account 
 UBISOFT_EMAIL = "your ubisoft email"
 UBISOFT_PASSWORD = "your ubisoft password"
-COMMAND_PREFIX = "your prefix"
+COMMAND_PREFIX = "."
